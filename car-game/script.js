2f1886c1cf71513a946267f9de4701597806f623
Stickman <157414990+StickmanL@users.noreply.github.com>
2024-08-15 08:51:15 -0700

    Add files via upload

---
+++ car-game/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('keydown', function(event) {
+    var player = document.getElementById('player');
+    var currentLeft = parseInt(window.getComputedStyle(player).left);
+
+    switch(event.key) {
+        case 'ArrowLeft':
+            player.style.left = (currentLeft - 35) + 'px';
+            break;
+        case 'ArrowRight':
+            player.style.left = (currentLeft + 35) + 'px';
+            break;
+    }
+});
+
+function speed() {
+    document.getElementById('speed').innerHTML = +1
+}
