548ed8badc171ec8b2855215099888ffad214af0
rasz64 <157414990+rasz64@users.noreply.github.com>
2024-11-27 16:28:01 +0100

    Update script.js

--- ball-clicker/script.js
+++ ball-clicker/script.js
@@ -7,11 +7,3 @@ circle.addEventListener("click", function() {
     score++;
     scoreElement.textContent = score;
 });
-
-function save() {
-    doument.getElementById("score").value = localStorage.setItem("data", saveInput;
-}
-
-function load() {
-    document.getElementById("score").innerHTML = localStorage.getItem("data");
-}
